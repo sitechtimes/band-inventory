6a04a5ae5106595a393e29f95992c6de50bb73a9
katherine <116115224+kaerulean@users.noreply.github.com>
2025-04-21 19:27:43 -0400

    asdasfasga

--- band-inventory/src/main.js
+++ band-inventory/src/main.js
@@ -2,5 +2,9 @@ import './assets/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './routes/index.js';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+
+app.mount('#app')
