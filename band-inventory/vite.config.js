fafe005ef718053cadca80445e2693c1690f6ffb
katherine <116115224+kaerulean@users.noreply.github.com>
2025-04-23 10:17:02 -0400

    i broke it :skull:

--- band-inventory/vite.config.js
+++ band-inventory/vite.config.js
@@ -5,9 +5,10 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
+  pluginnps: [
     vue(),
     vueDevTools(),
   ],
