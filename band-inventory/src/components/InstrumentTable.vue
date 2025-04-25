587b19cd88cb5360e709c0ca2843e4d2b78a0a9e
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2025-04-24 23:23:20 -0400

    table

---
+++ band-inventory/src/components/InstrumentTable.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="p-4">
+    <h1 class="font-bold my-8 text-xl">Instrument Listing</h1>
+    <div class="overflow-x-auto">
+        <table class="table">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Category</th>
+                    <th>Section</th>
+                    <th>Serial Model</th>
+                    <th>Case Number</th>
+                    <th>Manufacturer</th>
+                    <th>SI Tech HS ID</th>
+                    <th>Assigned to</th>
+                    <th>Condition</th>
+                    <th>Year of Purchase</th>
+                    <th>Barcode</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- row 1 -->
+                <tr v-for="instrument in instruments" :key="instrument.id">
+                    <td>{{ instrument.category }}</td>
+                    <td>{{ instrument.section }}</td>
+                    <td>{{ instrument.serial_model }}</td>
+                    <td>{{ instrument.case_number }}</td>
+                    <td>{{ instrument.manufacturer }}</td>
+                    <td>{{ instrument.sitech_hs_id }}</td>
+                    <td>{{ instrument.assigned_to }}</td>
+                    <td>{{ instrument.condition }}</td>
+                    <td>{{ instrument.year_of_purchase }}</td>
+                    <td>{{ instrument.barcode }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const instruments = ref([])
+
+async function getInstruments() {
+    const { data } = await supabase.from('instruments').select()
+    instruments.value = data
+    console.log(data)
+}
+
+onMounted(() => {
+    getInstruments()
+})
+</script>
