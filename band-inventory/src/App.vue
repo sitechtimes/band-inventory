745cc925ff5a55afeb4d091be2647e20414475c2
katherine <116115224+kaerulean@users.noreply.github.com>
2025-05-01 22:51:15 -0400

    typescript installed

--- band-inventory/src/App.vue
+++ band-inventory/src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 // import { ref, onMounted } from 'vue'
 // import { supabase } from './lib/supabaseClient'
 
@@ -12,12 +12,23 @@
 // onMounted(() => {
 //    getInstruments()
 // })
+
+// import { ref } from 'vue'
+// import type { Ref } from 'vue'
+
+// const year: Ref<string | number> = ref('2020')
+
+// year.value = 2020 // ok!
+// console.log(year.value)
+
+
 </script>
 
 <template>
   <!-- <ul>
     <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.category }}</li>
   </ul> -->
+  <h1>asdh</h1>
 </template>
 
 <style></style>
