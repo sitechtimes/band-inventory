73ac24d6bf0ae92a838371503e42719bb504fa06
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2025-05-29 11:01:57 -0400

    merge main

--- band-inventory/src/App.vue
+++ band-inventory/src/App.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import navBar from "./components/navBar.vue";
+// import { ref, onMounted } from 'vue'
+// import { supabase } from './lib/supabaseClient'
+
+// const instruments = ref([])
+
+// async function getInstruments() {
+//   const { data } = await supabase.from('instruments').select()
+//   instruments.value = data
+// }
+
+// onMounted(() => {
+//    getInstruments()
+// })
+
+// import { ref } from 'vue'
+// import type { Ref } from 'vue'
+
+// const year: Ref<string | number> = ref('2020')
+
+// year.value = 2020 // ok!
+// console.log(year.value)
 </script>
 
 <template>
