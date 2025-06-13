84965ab24f931d07687747ca1aef8a893259ad30
katherine <katherineze26@gmail.com>
2025-06-12 20:59:24 -0400

    asd

--- band-inventory/src/pages/home.vue
+++ band-inventory/src/pages/home.vue
@@ -1,9 +1,11 @@
 <template>
   <InstrumentTable></InstrumentTable>
+  <filter></filter>
 </template>
 
 <script setup lang="ts">
 import InstrumentTable from '../components/InstrumentTable.vue'
+import filter from "../components/filter.vue";
 </script>
 
 <style></style>
