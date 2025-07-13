cccba95217b99b40131dc73bc67244107de58337
katherine <katherineze26@gmail.com>
2025-07-13 16:09:13 -0400

    asd

--- band-inventory/src/pages/home.vue
+++ band-inventory/src/pages/home.vue
@@ -1,11 +1,37 @@
 <template>
-  <navBar></navBar>
-  <InstrumentTable></InstrumentTable>
+  <div v-if="isUserNull">
+    <span>You don't have access to this page. Please sign in 
+      <RouterLink to="/" class="underline text-blue-600">here</RouterLink>
+      .
+    </span>
+    
+  </div>
+  <div v-else>
+    <navBar></navBar>
+    <InstrumentTable></InstrumentTable>
+  </div>
 </template>
 
 <script setup lang="ts">
 import navBar from '@/components/navBar.vue';
 import InstrumentTable from '../components/InstrumentTable.vue'
+import { onMounted, ref } from 'vue';
+import { useUserStore } from "@/stores/authStore";
+import { RouterLink } from 'vue-router';
+
+const isUserNull = ref(true)
+const userStore = useUserStore()
+
+function checkAccess(){
+  if (userStore.user != null){
+    isUserNull.value =! isUserNull.value
+  }
+}
+
+onMounted(() => {
+  checkAccess()
+})
+
 </script>
 
 <style></style>
