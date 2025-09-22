321ac7d1767dc1fda4d357aed79e5ce3b367d8a4
katie <113192783+katiekhamarkhanova@users.noreply.github.com>
2025-09-22 07:58:26 -0700

    navbar

--- band-inventory/src/components/navBar.vue
+++ band-inventory/src/components/navBar.vue
@@ -23,32 +23,16 @@
             tabindex="0"
             class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li><a>Item 1</a></li>
-            <li>
-              <a>Parent</a>
-              <ul class="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+            <li><a>Home</a></li>
+            <li><a>Add Instruments</a></li>
           </ul>
         </div>
-        <a class="btn btn-ghost text-xl">daisyUI</a>
+        <a class="btn btn-ghost text-xl">Band Inventory</a>
       </div>
       <div class="navbar-center hidden lg:flex">
         <ul class="menu menu-horizontal px-1">
-          <li><a>Item 1</a></li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul class="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </details>
-          </li>
-          <li><a>Item 3</a></li>
+          <li><a>Home</a></li>
+          <li><a>Add Instruments</a></li>
         </ul>
       </div>
       <div class="navbar-end">
